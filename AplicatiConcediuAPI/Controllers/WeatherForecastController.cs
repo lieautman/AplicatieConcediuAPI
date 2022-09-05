091d716057a6ef0169801ce8af90305ccdf29624
lieautman <dumi_cristi@yahoo.com>
2022-09-05 09:53:12 +0300

    Add project files.

---
+++ AplicatiConcediuAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using XD.Models;
+
+namespace XD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly GameOfThronesContext _gameOfThronesContext;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, GameOfThronesContext gameOfThronesContext)
+        {
+            _logger = logger;
+           _gameOfThronesContext = gameOfThronesContext;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public List<Concediu> Get()
+        {
+            List<Concediu> a = _gameOfThronesContext.Concedius.Include(x => x.TipConcediu).Select(x => new Concediu() { Id = x.Id, Comentarii = x.Comentarii, TipConcediu = x.TipConcediu }).Where(x=>x.TipConcediu.Id==2).ToList();
+
+            
+            return a;
+        }
+
+        [HttpPost(Name = "GetWeatherForecast")]
+        public List<Concediu> Post()
+        {
+            List<Concediu> a = _gameOfThronesContext.Concedius.Include(x => x.TipConcediu).Select(x => new Concediu() { Id = x.Id, Comentarii = x.Comentarii, TipConcediu = x.TipConcediu }).ToList();
+
+            List<Concediu> b = new List<Concediu>();
+            for (long i = 0; i < a.LongCount(); i++)
+            {
+                Concediu conc = new Concediu(a[Int32.Parse(i.ToString())].Id, a[Int32.Parse(i.ToString())].TipConcediuId, a[Int32.Parse(i.ToString())].DataInceput, a[Int32.Parse(i.ToString())].DataSfarsit, a[Int32.Parse(i.ToString())].InlocuitorId, a[Int32.Parse(i.ToString())].Comentarii, a[Int32.Parse(i.ToString())].StareConcediuId, a[Int32.Parse(i.ToString())].AngajatId, a[Int32.Parse(i.ToString())].Angajat, a[Int32.Parse(i.ToString())].Inlocuitor, a[Int32.Parse(i.ToString())].StareConcediu, a[Int32.Parse(i.ToString())].TipConcediu);
+                b.Add(conc);
+            }
+            return b;
+        }
+        [HttpPut(Name = "GetWeatherForecast")]
+        public List<Concediu> Put()
+        {
+            return _gameOfThronesContext.Concedius.Include(x => x.TipConcediu).Select(x => new Concediu() { Id = x.Id, Comentarii = x.Comentarii, TipConcediu = x.TipConcediu }).ToList();
+        }
+        [HttpDelete(Name = "GetWeatherForecast")]
+        public List<Concediu> Delete()
+        {
+            return _gameOfThronesContext.Concedius.Include(x => x.TipConcediu).Select(x => new Concediu() { Id = x.Id, Comentarii = x.Comentarii, TipConcediu = x.TipConcediu }).ToList();
+        }
+    }
+}
---
+++ AplicatiConcediuAPI/Models/Concediu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XD.Models
+{
+    public partial class Concediu
+    {
+        public int Id { get; set; }
+        public int? TipConcediuId { get; set; }
+        public DateTime DataInceput { get; set; }
+        public DateTime DataSfarsit { get; set; }
+        public int? InlocuitorId { get; set; }
+        public string? Comentarii { get; set; }
+        public int? StareConcediuId { get; set; }
+        public int? AngajatId { get; set; }
+
+        public virtual Angajat? Angajat { get; set; }
+        public virtual Angajat? Inlocuitor { get; set; }
+        public virtual StareConcediu? StareConcediu { get; set; }
+        public virtual TipConcediu? TipConcediu { get; set; }
+
+        public Concediu() { }
+
+        public Concediu(int id, int? tipConcediuId, DateTime dataInceput, DateTime dataSfarsit, int? inlocuitorId, string? comentarii, int? stareConcediuId, int? angajatId, Angajat? angajat, Angajat? inlocuitor, StareConcediu? stareConcediu, TipConcediu? tipConcediu)
+        {
+            Id = id;
+            TipConcediuId = tipConcediuId;
+            DataInceput = dataInceput;
+            DataSfarsit = dataSfarsit;
+            InlocuitorId = inlocuitorId;
+            Comentarii = comentarii;
+            StareConcediuId = stareConcediuId;
+            AngajatId = angajatId;
+            Angajat = angajat;
+            Inlocuitor = inlocuitor;
+            StareConcediu = stareConcediu;
+            TipConcediu = tipConcediu;
+        }
+    }
+}
