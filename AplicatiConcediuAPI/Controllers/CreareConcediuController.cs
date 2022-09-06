0cc9bb6ff4a468c492faa34ba29a06a7113e58c0
Teodor Ungureanu <teodor.ungureanu@totalsoft.ro>
2022-09-06 16:44:40 +0300

    update

---
+++ AplicatiConcediuAPI/Controllers/CreareConcediuController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XD.Models;
+
+namespace AplicatieConcediuAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CreareConcediuController : Controller
+    {
+        private readonly ILogger<CreareConcediuController> _logger;
+        private readonly GameOfThronesContext _gameOfThronesContext;
+        public CreareConcediuController(ILogger<CreareConcediuController> logger, GameOfThronesContext gameOfThronesContext)
+        {
+            _logger = logger;
+            _gameOfThronesContext = gameOfThronesContext;
+        }
+        [HttpGet("GetInlocuitor/{AngajatId}")]
+        public List<Angajat> GetInlocuitori(int AngajatId)
+        {
+
+            Angajat angajatCurent = _gameOfThronesContext.Angajats.Where(x => x.Id == AngajatId).FirstOrDefault();
+            if (angajatCurent == null)
+                return null;
+            List<Angajat> _inlocuitori = _gameOfThronesContext.Angajats.Select(a => new Angajat() {Prenume = a.Prenume ,Nume = a.Nume, Id = a.Id, IdEchipa = a.IdEchipa }).Where(a => a.IdEchipa == angajatCurent.IdEchipa && a.Id != angajatCurent.Id).ToList();
+            return
+                _inlocuitori;
+        }
+        [HttpGet("TipuriConcediu")]
+        public List<TipConcediu> GetTipuriConcediu()
+        {
+            List<TipConcediu> _tipConcediu = _gameOfThronesContext.TipConcedius.Select(a => new TipConcediu() { Nume = a.Nume, Id = a.Id}).ToList();
+
+            return
+                   _tipConcediu;
+        }
+
+        [HttpPost("PostConcediu")]
+        public ActionResult PostConcediu([FromBody] Concediu c)
+
+        {
+        
+            _gameOfThronesContext.Concedius.Add(c);
+            _gameOfThronesContext.SaveChanges();
+
+            return Ok();
+
+        }
+    }
+}
