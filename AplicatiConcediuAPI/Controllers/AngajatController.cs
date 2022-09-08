1200d7674a8ec63957f1181bc04f9063f8d9b4a6
lieautman <dumi_cristi@yahoo.com>
2022-09-08 15:42:51 +0300

    adaugat ehcipe la toti angajatii

--- AplicatiConcediuAPI/Controllers/AngajatController.cs
+++ AplicatiConcediuAPI/Controllers/AngajatController.cs
@@ -395,7 +395,7 @@ namespace AplicatieConcediuAPI.Controllers
         public ActionResult<List<Angajat>> GetPreluareDateDespreTotiAngajatii()
         {
             List<Angajat> conc = new List<Angajat>();
-            conc = _gameOfThronesContext.Angajats.Include(x=>x.Manager).Select(x => x).ToList();
+            conc = _gameOfThronesContext.Angajats.Include(x=>x.Manager).Include(x=>x.IdEchipaNavigation).Select(x => x).ToList();
             if (conc != null) { return Ok(conc); }
             return NoContent();
         }
@@ -403,7 +403,7 @@ namespace AplicatieConcediuAPI.Controllers
         public ActionResult<List<Angajat>> PostPreluareDateDespreTotiAngajatiiDinEchipa(Angajat a)
         {
             List<Angajat> conc = new List<Angajat>();
-            conc = _gameOfThronesContext.Angajats.Include(x=>x.Manager).Select(x => x).Where(x => x.IdEchipa == a.IdEchipa).ToList();
+            conc = _gameOfThronesContext.Angajats.Include(x=>x.Manager).Include(x => x.IdEchipaNavigation).Select(x => x).Where(x => x.IdEchipa == a.IdEchipa).ToList();
             if (conc != null) { return Ok(conc); }
             return NoContent();
         }
