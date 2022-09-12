db40f6fb7e55cbe777c75938d3ed44b7547eef0f
lieautman <dumi_cristi@yahoo.com>
2022-09-12 11:22:01 +0300

    update

--- AplicatiConcediuAPI/Controllers/AngajatController.cs
+++ AplicatiConcediuAPI/Controllers/AngajatController.cs
@@ -483,20 +483,56 @@ namespace AplicatieConcediuAPI.Controllers
 
         //USE: Pagina toti angajatii (TotiAngajatii)
         //formular toti angajatii preluare date angajati
-        [HttpGet("GetPreluareDateDespreTotiAngajatii")]
-        public ActionResult<List<Angajat>> GetPreluareDateDespreTotiAngajatii()
+        [HttpGet("GetPreluareDateDespreTotiAngajatii/{index1}/{index2}")]
+        public ActionResult<List<Angajat>> GetPreluareDateDespreTotiAngajatii(int index1, int index2)
         {
             List<Angajat> conc = new List<Angajat>();
             conc = _gameOfThronesContext.Angajats.Include(x=>x.Manager).Include(x=>x.IdEchipaNavigation).Select(x => x).ToList();
-            if (conc != null) { return Ok(conc); }
+            if (conc != null) 
+            {
+                if (index2 > conc.Count)
+                    index2 = conc.Count;
+                return Ok(conc.GetRange(index1,index2));
+            }
             return NoContent();
         }
-        [HttpPost("PostPreluareDateDespreTotiAngajatiiDinEchipa")]
-        public ActionResult<List<Angajat>> PostPreluareDateDespreTotiAngajatiiDinEchipa(Angajat a)
+        [HttpGet("GetPreluareNumarDePagini/{index1}/{index2}")]
+        public ActionResult<int> GetPreluareNumarDePagini(int index1, int index2)
+        {
+            List<Angajat> conc = new List<Angajat>();
+            conc = _gameOfThronesContext.Angajats.Include(x => x.Manager).Include(x => x.IdEchipaNavigation).Select(x => x).ToList();
+            if (conc != null)
+            {
+                if (index2 > conc.Count)
+                    index2 = conc.Count;
+                return Ok(conc.Count/(index2-index1));
+            }
+            return NoContent();
+        }
+        [HttpPost("PostPreluareDateDespreTotiAngajatiiDinEchipa/{index1}/{index2}")]
+        public ActionResult<List<Angajat>> PostPreluareDateDespreTotiAngajatiiDinEchipa([FromBody] Angajat a,int index1, int index2)
         {
             List<Angajat> conc = new List<Angajat>();
             conc = _gameOfThronesContext.Angajats.Include(x=>x.Manager).Include(x => x.IdEchipaNavigation).Select(x => x).Where(x => x.IdEchipa == a.IdEchipa).ToList();
-            if (conc != null) { return Ok(conc); }
+            if (conc != null)
+            {
+                if (index2 > conc.Count)
+                    index2 = conc.Count;
+                return Ok(conc.GetRange(index1, index2));
+            }
+            return NoContent();
+        }
+        [HttpGet("PostPreluareNumarDePaginiDinEchipa/{index1}/{index2}")]
+        public ActionResult<int> PostPreluareNumarDePaginiDinEchipa([FromBody] Angajat a,int index1, int index2)
+        {
+            List<Angajat> conc = new List<Angajat>();
+            conc = _gameOfThronesContext.Angajats.Include(x => x.Manager).Include(x => x.IdEchipaNavigation).Select(x => x).Where(x => x.IdEchipa == a.IdEchipa).ToList();
+            if (conc != null)
+            {
+                if (index2 > conc.Count)
+                    index2 = conc.Count;
+                return Ok(conc.Count / (index2 - index1));
+            }
             return NoContent();
         }
 
